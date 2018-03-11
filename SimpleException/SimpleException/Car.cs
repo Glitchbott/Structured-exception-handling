d5097ac0a5379953acffb41a7d3f35358322d94a
Glitchbott <36093622+Glitchbott@users.noreply.github.com>
2018-03-11 17:20:02 +0600

    Base class System.Exception

---
+++ SimpleException/SimpleException/Car.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimpleException
+{
+    class Car
+    {
+        // максимальн скорость
+        public const int MaxSpeed = 100;
+        // св-ва автомобиля
+        public int CurrentSpeed { get; set; }
+        public string PetName { get; set; }
+        // не вышел ли автомобильиз стоя?
+        public bool carDead;
+        // автомобиль имеет радиоприемник
+        private Radio MusicBox = new Radio();
+        //конструкторы
+        public Car() {}
+        public Car(string name, int speed)
+        {
+            PetName = name;
+            CurrentSpeed = speed;
+        }
+        public void CrankTunes(bool state)
+        {
+            // делегировать запрос внутреннему объекту
+            MusicBox.TurnOn(state);
+        }
+        // Проверка не сдох ли автомобиль
+        public void Accelerate(int delta)
+        {
+            if (carDead)
+                Console.WriteLine("{0}  is out of order...", PetName);
+            else
+            {
+                CurrentSpeed += delta;
+
+                if(CurrentSpeed > MaxSpeed)
+                {
+                    Console.WriteLine("{0}  has over !", PetName);
+                    CurrentSpeed = 0;
+                    carDead = true;
+                }
+                else
+                    Console.WriteLine("=>  CurrenSpeed = {0}", CurrentSpeed);
+            }
+        }
+            
+    }
+}
---
+++ SimpleException/SimpleException/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimpleException
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("**** Simple Expection Example****\n");
+            Console.WriteLine("=>  Creating a car and stepping on it!");
+            Car myCar = new Car("Zip", 10);
+            myCar.CrankTunes(true);
+            for (int i = 0; i < 10; i++)
+                myCar.Accelerate(10);
+            Console.ReadLine();
+        }
+    }
+}
